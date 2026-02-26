12cd29071b550af08a9ebf6bb6af94d0d5fde491
yehor <yehorbe@gmail.com>
2026-02-26 11:11:44 +0200

    2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ connection = mariadb.connect(
 print("Connected!")
 
 def get_countries():
-    sql = "SELECT name, iso_country FROM country WHERE continent = EU AND iso_country != 'RU'"
+    sql = "SELECT name, iso_country FROM country WHERE continent = EU AND iso_country != 'RU'"""
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -25,20 +25,37 @@ def get_goals():
     result = cursor.fetchall()
     return result
 
-
-def creat_game(player_name,role):
-    start_money=5000
-    start_location="FI"
-    sql = "INSERT INTO game (player_name, role, money, location) VALUES (%s, %s, %s, %s);
+def get_items():
+    sql = "SELECT * FROM items;"
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(sql, (player_name, role, start_money, start_location))
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+def creat_game(start_money, p_name, p_role, cur_country):
+    sql = "INSERT INTO game (money, player_name, role, location) VALUES (%s, %s, %s, %s)";
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (p_name, p_role, start_money, cur_country,))
     game_id = cursor.lastrowid
-    return game_id, start_money, start_location
+    goals = get_goals()
+    goal_list = []
+    for goal in goals:
+        for i in range(0, goal['probability'], 1):
+            goal_list.append(goal['id'])
+
+    g_countries = a_ports[1:].copy()
+    random.shuffle(g_ports)
+
+    for i, goal_id in enumerate(goal_list):
+        sql = "INSERT INTO ports (game, airport, goal) VALUES (%s, %s, %s);"
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(sql, (g_id, g_ports[i]['ident'], goal_id))
+    return game_id
 
 test_player = "TEST_PLAYER"
 test_role = "regular"
 
 game_id, money, location = creat_game(test_player, test_role)
-print("game_id:", game_id)
+print("Game ID:", game_id)
 print("Start money:", money)
 print("Start location:", location)
