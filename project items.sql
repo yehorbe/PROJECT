75ea349fa54ddca9545cd695132263961bf360ca
Amuro78 <Amuro0101@outlook.com>
2026-02-26 12:45:04 +0200

    Add files via upload

---
+++ project items.sql
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+Country: Andorra, code: AD, item: cigar, Number: 1
+Country: Albania, code: AL, item: handmade carpets, Number: 2
+Country: Austria, code: AT, item: mozartkugel, Number: 3
+Country: Bosnia and Herzegovina, code: BA, item: copper coffee set, Number: 4
+Country: Belgium, code: BE, item: handmade chocolate, Number: 5
+Country: Bulgaria, code: BG, item: Rose essential oil, Number: 6
+Country: Belarus, code: BY, item:linen clothing, Number: 7
+Country: Switzerland, code: CH, item: high-end watches, Number: 8
+Country: Czech Republic, code: CZ, item: czech crystal, Number: 9
+Country: Germany, code: DE, item: dark beer, Number: 10
+Country: Denmark, code: DK, item: royal porcelain, Number: 11
+Country: Estonia, code: EE, item: amber jewelry, Number: 12
+Country: Spain, code: ES,item: Iberian ham, Number: 13
+Country: Finland, code: FI, item: Moomin doll Number: 14
+Country: Faroe Islands, code: FO, item: dried fish Number: 15
+Country: France, code: FR, item: High-end skincare products, Number: 16
+Country: United Kingdom, code: GB, item: Bone China Tea Sets, Number: 17
+Country: Guernsey, code: GG, item: abalone, Number: 18
+Country: Gibraltar, code: GI, item: olive oil, Number: 19
+Country: Greece, code: GR, item: handmade leather sandals, Number: 20
+Country: Croatia, code: HR, item: red coral jewelry, Number: 21
+Country: Hungary, code: HU, item: Fruit Brandy, Number: 22
+Country: Ireland, code: IE, item: Irish whiskey, Number: 23
+Country: Isle of Man, code: IM, item: Queen Scallop, Number: 24
+Country: Iceland, code: IS, item: handmade candles, Number: 25
+Country: Italy, code: IT, item: handmade leather bags, Number: 26
+Country: Jersey, code: JE, item: black butter, Number: 27
+Country: Liechtenstein, code: LI, item: commemorative stamps, Number: 28
+Country: Lithuania, code: LT, item: Wood carving, Number: 29
+Country: Luxembourg, code: LU, item: grape wine, Number: 30
+Country: Latvia, code: LV, item: black balsam, Number: 31
+Country: Monaco, code: MC, item: monaco essential oil Number: 32
+Country: Moldova, code: MD, item: golden millet Number: 33
+Country: Montenegro, code: ME, item: truffle sauce Number: 34
+Country: North Macedonia, code: MK, item: Hand-painted porcelain tiles, Number: 35
+Country: Malta, code: MT, item: malta knights statue, Number: 36
+Country: Netherlands, code: NL, item: wooden shoes, Number: 37
+Country: Norway, code: NO, item: deep-sea fish oil,  Number: 38
+Country: Poland, code: PL, item: kabanos, Number: 39
+Country: Portugal, code: PT, item: royal Soap, Number: 40
+Country: Romania, code: RO, item: pipe, Number: 41
+Country: Serbia, code: RS, item: honey Number: 42
+Country: Sweden, code: SE, item: handmade silver jewelry， Number: 43
+Country: Slovenia, code: SI, item: Idrija Lace, Number: 44
+Country: Slovakia, code: SK, item: herbal bitter wine, Number: 45
+Country: San Marino, code: SM, item: cheese, Number: 46
+Country: Ukraine, code: UA, item: Vodka, Number: 47
+Country: Vatican City, code: VA, item: holy grail, Number: 48
+Country: Kosovo, code: XK, item: stringed instrument Number: 49
