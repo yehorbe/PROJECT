ddb127127d331b9247c241faefb0ea56a81beead
Jolyaa112 <jolyaa@metropolia.fi>
2026-02-26 10:50:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ connection = mariadb.connect(
 print("Connected!")
 
 def get_countries():
-    sql = "SELECT name, iso_country FROM country WHERE continent = EU AND iso_country != 'RU'"""
+    sql = "SELECT name, iso_country FROM country WHERE continent = EU AND iso_country != 'RU'"
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -24,3 +24,21 @@ def get_goals():
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
+
+
+def creat_game(player_name,role):
+    start_money=5000
+    start_location="FI"
+    sql = "INSERT INTO game (player_name, role, money, location) VALUES (%s, %s, %s, %s);
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql, (player_name, role, start_money, start_location))
+    game_id = cursor.lastrowid
+    return game_id, start_money, start_location
+
+test_player = "TEST_PLAYER"
+test_role = "regular"
+
+game_id, money, location = creat_game(test_player, test_role)
+print("game_id:", game_id)
+print("Start money:", money)
+print("Start location:", location)
